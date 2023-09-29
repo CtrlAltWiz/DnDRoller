5b85d1317a9ed378b2e456bb69d3b1ca8cdc5af7
CtrlAltWiz <46701041+CtrlAltWiz@users.noreply.github.com>
2023-09-29 11:19:35 -0700

    Add files via upload
    Added optional features for modifying rolls with an added/subtracted value.
    
    Fixed an issue where the total sum of rolls was not being tallied/reset correctly.

--- DnD_Roller.py
+++ DnD_Roller.py
@@ -6,9 +6,14 @@ import random
 # by CtrlAltWiz
 # https://github.com/CtrlAltWiz
 
+# Initialize a dictionary to store roll results
+roll_results = {}
+
 def roll_dice():
     selected_dice = dice_combobox.get()
     num_rolls = int(rolls_entry.get())
+    roll_modifier = int(modifier_entry.get())
+    modifier_operation = modifier_operation_combobox.get()
 
     if selected_dice == "":
         result_label.config(text="Please select a dice type.")
@@ -16,12 +21,33 @@ def roll_dice():
 
     sides = int(selected_dice.split("d")[1])
 
-    results = [random.randint(1, sides) for _ in range(num_rolls)]
-    result_label.config(text=f"You rolled a {selected_dice} {num_rolls} times: {results}")
+    # Roll the dice and store the original results
+    original_results = [random.randint(1, sides) for _ in range(num_rolls)]
 
+    # Apply the roll modifier based on the selected operation
+    if modifier_operation == "+":
+        results_with_modifier = [result + roll_modifier for result in original_results]
+    else:
+        results_with_modifier = [result - roll_modifier for result in original_results]
+
+    # Store the results in the dictionary
+    if selected_dice in roll_results:
+        roll_results[selected_dice].extend(results_with_modifier)
+    else:
+        roll_results[selected_dice] = results_with_modifier
+
+    result_label.config(text=f"You rolled a {selected_dice} {num_rolls} times, with a modifier of {roll_modifier} ({modifier_operation}):\n"
+                            f"Original results: {original_results}\n"
+                            f"Modified results: {results_with_modifier}\n"
+                            f"Total {selected_dice} rolls with modifier: {roll_results[selected_dice]}")
+
+def reset_tally():
+    roll_results.clear()
+    result_label.config(text="Total reset.")
+    
 # Create the main window
 window = tk.Tk()
-window.title("Dice Roller")
+window.title("D&D Dice Roller")
 
 # Create and configure widgets
 dice_label = ttk.Label(window, text="Select Dice:")
@@ -31,7 +57,15 @@ dice_combobox = ttk.Combobox(window, values=dice_options)
 rolls_label = ttk.Label(window, text="Number of Rolls:")
 rolls_entry = ttk.Entry(window)
 
+modifier_label = ttk.Label(window, text="Roll Mod:")
+modifier_entry = ttk.Entry(window)
+
+modifier_operation_label = ttk.Label(window, text="Modifier:")
+modifier_operation_options = ["+", "-"]
+modifier_operation_combobox = ttk.Combobox(window, values=modifier_operation_options)
+
 roll_button = ttk.Button(window, text="Roll", command=roll_dice)
+reset_button = ttk.Button(window, text="Reset Total", command=reset_tally)
 result_label = ttk.Label(window, text="")
 
 # Place widgets on the window using grid layout
@@ -39,8 +73,13 @@ dice_label.grid(row=0, column=0)
 dice_combobox.grid(row=0, column=1)
 rolls_label.grid(row=1, column=0)
 rolls_entry.grid(row=1, column=1)
-roll_button.grid(row=2, columnspan=2)
-result_label.grid(row=3, columnspan=2)
+modifier_label.grid(row=2, column=0)
+modifier_entry.grid(row=2, column=1)
+modifier_operation_label.grid(row=3, column=0)
+modifier_operation_combobox.grid(row=3, column=1)
+roll_button.grid(row=4, column=0)
+reset_button.grid(row=4, column=1)
+result_label.grid(row=5, columnspan=2)
 
 # Start the Tkinter main loop
 window.mainloop()
